294b04e72ddc16dad865059b0a214cf65734cd00
Ying - a buddhist developer <fyanton@hotmail.com>
2021-07-28 20:07:51 -0700

    add concatenate strings

---
+++ concatenate_strings.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# to concatenate strings and numbers
+
+s = 'aaa' + 'bbb' + 'ccc'
+print(s)
+
+s1 = 'aaa'
+s2 = 'bbb'
+s3 = 'ccc'
+s4 = 'ddd'
+
+s = s1 + s2 + s3 + s4  
+print(s)
+
+
+s = s1 + '.' + s2 + '.' + s3 + '.' + s4
+print(s)
